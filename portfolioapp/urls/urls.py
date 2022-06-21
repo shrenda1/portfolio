718ce820d567796bf9d145e53cf7cbdfa9dae104
Aayush Shrestha <aayuhs2021shrestha@gmail.com>
2022-06-21 20:34:25 +0545

    second commit

--- portfolioapp/urls/urls.py
+++ portfolioapp/urls/urls.py
@@ -1,7 +1,7 @@
 from django import views
 from django.urls import URLPattern, path
-from portfolioapp import views
+from portfolioapp.views import home_controller, cv_controller
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('cv', views.cv, name = "cv"),
+    path('', home_controller.home, name="home"),
+    path('cv', cv_controller.cv, name = "cv"),
 ]
---
+++ portfolioapp/views/cv_controller.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+def cv(request):
+    
+    return render(request, "CV.html")
