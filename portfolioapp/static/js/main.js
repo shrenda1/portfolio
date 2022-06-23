5b3be427ba8fb0d18fe5488079b8baa587bf9769
Aayush Shrestha <aayuhs2021shrestha@gmail.com>
2022-06-23 15:12:16 +0545

    menu bar

---
+++ portfolioapp/static/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(()=>{
+    $('.menu-bar').mouseenter(()=>{
+        $('.menu-bar').css('opacity', 1);
+    })
+    $('.menu-bar').mouseleave(()=>{
+        $('.menu-bar').css('opacity', 0.4);
+    })
+})
